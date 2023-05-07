13db61dbefbcbc9c4105f6404bf524afc7635f03
RaulDM220 <rborrego@datamart.cl>
2023-05-07 12:24:24 -1100

    crating a server wiyh express

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-console.log('Hello World')
+const express = require('express')
 
-console.log(5+8)
+//Create app
+const app = express()
+
+//Routing
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/we', (req, res) => {
+    res.json({ message: 'Our page'})
+})
+
+//Define a port and run the project
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server runing at port: ${port}`)
+})
