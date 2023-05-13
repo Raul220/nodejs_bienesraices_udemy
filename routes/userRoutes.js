9ca131911acdab8aa1557f0536f50174336f0d55
RaulDM220 <rborrego@datamart.cl>
2023-05-12 22:45:40 -1100

    connect rout view and controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { forgotPasswordForm, loginForm, registryForm } from '../controllers/userController.js'
+import { forgotPasswordForm, loginForm, registryForm, registry } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.get('/login', loginForm)
+
 router.get('/registry', registryForm)
+router.post('/registry', registry)
+
 router.get('/forgot-password', forgotPasswordForm)
 
 export default router
