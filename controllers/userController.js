4d79e2ca21571ae6c87362f61b20895661bb362a
RaulDM220 <rborrego@datamart.cl>
2023-05-13 22:44:22 -1100

    auto fill ok fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,20 +23,26 @@ const registry = async (req, res) => {
     .isLength({ min: 6 })
     .withMessage("El password debe tener al menos 6 caracteres.")
     .run(req);
-  await check("confirm_password")
-    .equals("password")
-    .withMessage("Las contraseñas no son iguales")
-    .run(req);
+  if (req.body["confirm_password"] !== req.body["password"]) {
+    await check("confirm_password")
+      .equals("password")
+      .withMessage("Las contraseñas no son iguales")
+      .run(req);
+  }
 
   let result = validationResult(req);
 
   if (!result.isEmpty()) {
     return res.render("auth/registry", {
       page: "Crear cuenta",
-      errors: result.array()
+      errors: result.array(),
+      user: {
+        name: req.body.name,
+        email: req.body.email,
+      },
     });
   }
-  
+
   const user = await User.create(req.body);
   res.json(user);
 };
