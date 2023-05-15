6e5555a5cec6044417662474468aa4f4278f5b84
RaulDM220 <rborrego@datamart.cl>
2023-05-14 18:38:00 -1100

    send email nodemailer & mailtrap

---
+++ helpers/emails.js
@@ -0,0 +1,12 @@
+const registryEmail = async (data) => {
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+};
+
+export { registryEmail };
