049320a0487bd04e780957d1a849ea4205e0fbaa
RaulDM220 <rborrego@datamart.cl>
2023-05-16 17:59:50 -1100

    start property form

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,3 +1,8 @@
+/**
+ * Renderiza la vista de propiedades
+ * @param {*} req peticion
+ * @param {*} res respuesta
+ */
 const admin = (req, res) => {
   res.render("properties/admin", {
     page: "Propiedades",
@@ -5,4 +10,16 @@ const admin = (req, res) => {
   });
 };
 
-export { admin };
+/**
+ * Renderiza la vista de crear propiedad
+ * @param {*} req peticion
+ * @param {*} res respuesta
+ */
+const create = (req, res) => {
+  res.render("properties/create", {
+    page: "Crear propiedad",
+    bar: true,
+  });
+};
+
+export { admin, create };
