a55cc9cf591e669ebb50321a6d0a3b3f4fbe09aa
RaulDM220 <rborrego@datamart.cl>
2023-05-24 12:57:53 -1100

    detail

---
+++ src/js/showMap.js
@@ -0,0 +1,15 @@
+(function () {
+  const lat = document.querySelector("#lat").textContent;
+  const lng = document.querySelector("#lng").textContent;
+  const map = L.map("map").setView([lat, lng], 16);
+  const street = document.querySelector("#street").textContent;
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  //Add pin
+
+  L.marker([lat, lng]).addTo(map).bindPopup(street);
+})();
